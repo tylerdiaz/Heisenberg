a99b3648efd0c73c77ac2c8d2f26c9aeb45c2eb1
mcr5fh <mcr5fh@virginia.edu>
2018-03-06 15:18:33 -0500

    Create folder structure

--- token/doctorVerificationToken.sol
+++ token/doctorVerificationToken.sol
@@ -30,7 +30,6 @@ contract ERC721Token is ERC721 {
   * @dev Guarantees msg.sender is owner of the given token
   * @param _tokenId uint256 ID of the token to validate its ownership belongs to msg.sender
   */
-
   modifier onlyOwnerOf(uint256 _tokenId) {
     require(ownerOf(_tokenId) == msg.sender);
     _;
